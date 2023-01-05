6ec4f5014b35ee3eae355339838638bfb2396e73
rohitsasi720 <rohitsasi720@gmail.com>
2023-01-05 16:50:19 +0530

    Fixed a logical potential bug

--- core/Database.php
+++ core/Database.php
@@ -12,35 +12,6 @@ class Database
     {
         $this->pdo = new PDO('mysql:host=' . DB_HOST . ';port=3306;', DB_USER, DB_PASSWORD);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       
-        $statement = $this->pdo->query("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'products';");
-        if(!$statement->fetchColumn()){
-            $statement = $this->pdo->prepare(
-                "USE php_mvc;
-                CREATE TABLE `products` (
-                    `sku` varchar(255) COLLATE utf8_bin NOT NULL,
-                    `name` varchar(255) COLLATE utf8_bin NOT NULL,
-                    `price` float NOT NULL,
-                    `type` varchar(255) COLLATE utf8_bin NOT NULL,
-                    `value` varchar(255) COLLATE utf8_bin NOT NULL
-                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-              
-                INSERT INTO `products` (`sku`, `name`, `price`, `type`, `value`) VALUES
-                    ('DGKE2334', 'Wings of Fire', 200, 'Book', 'Weight: 1 KG'),
-                    ('DTC240821', 'AMD', 1, 'DVD', 'Size: 300 MB'),
-                    ('GKS27290', 'Table', 90, 'Furniture', 'Dimensions: 40x20x15 CM');
-              
-              
-                ALTER TABLE `products`
-                    ADD PRIMARY KEY (`sku`);
-                COMMIT;"
-            );
-            
-            $statement->execute();
-            $statement->closeCursor(); 
-        }
-
-        
         
         $statement = $this->pdo->query("SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'php_mvc';");
         if (!$statement->fetchColumn()) {
@@ -68,6 +39,33 @@ class Database
             
             $statement->execute();
             $statement->closeCursor();
+        }
+
+        $statement = $this->pdo->query("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'products';");
+        if(!$statement->fetchColumn()){
+            $statement = $this->pdo->prepare(
+                "USE php_mvc;
+                CREATE TABLE `products` (
+                    `sku` varchar(255) COLLATE utf8_bin NOT NULL,
+                    `name` varchar(255) COLLATE utf8_bin NOT NULL,
+                    `price` float NOT NULL,
+                    `type` varchar(255) COLLATE utf8_bin NOT NULL,
+                    `value` varchar(255) COLLATE utf8_bin NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+              
+                INSERT INTO `products` (`sku`, `name`, `price`, `type`, `value`) VALUES
+                    ('DGKE2334', 'Wings of Fire', 200, 'Book', 'Weight: 1 KG'),
+                    ('DTC240821', 'AMD', 1, 'DVD', 'Size: 300 MB'),
+                    ('GKS27290', 'Table', 90, 'Furniture', 'Dimensions: 40x20x15 CM');
+              
+              
+                ALTER TABLE `products`
+                    ADD PRIMARY KEY (`sku`);
+                COMMIT;"
+            );
+            
+            $statement->execute();
+            $statement->closeCursor(); 
         }
 
         $this->pdo->query("USE php_mvc;");  
